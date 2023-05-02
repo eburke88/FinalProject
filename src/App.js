76664e174114b1afc97d97e319dd020c457cb35e
eburke88 <71228728+eburke88@users.noreply.github.com>
2023-05-02 19:56:51 -0400

    edit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import ProfilePage from './ProfilePage/ProfilePage.js'
 export default function App() {
   
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
           <Route index element={<ProfilePage />} />
           <Route path="ReXplore" element={<ReXplore />} />
